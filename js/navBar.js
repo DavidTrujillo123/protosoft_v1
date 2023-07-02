ed5d1a7428fa1bfa7ca66245bdc34f0da99a9d2b
DavidTrujillo123 <davot098@gmail.com>
2023-07-02 18:18:24 -0500

    Finalizacion de navBar mobile v1

---
+++ js/navBar.js
@@ -0,0 +1,6 @@
+const hamburgesa_menu = document.querySelector('.hamburgesa_menu');
+const general_nav = document.querySelector('.general_nav');
+const lanzarMenu = () =>{
+    general_nav.classList.toggle('inactive');
+}
+hamburgesa_menu.addEventListener('click', lanzarMenu);
