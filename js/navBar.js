091620c4883864052c63c32867d987296589b6e4
DavidTrujillo123 <davot098@gmail.com>
2023-07-09 23:40:57 -0500

    Creacion de pagina de home-user y adaptacion de navBar

--- js/navBar.js
+++ js/navBar.js
@@ -1,8 +1,22 @@
 const hamburgesa_menu = document.querySelector('.hamburgesa_menu');
 const general_nav = document.querySelector('.general_nav');
+const menu_registros = document.querySelector('.menu_registros');
+const menu_perfil = document.querySelector('.menu_perfil')
+const nav_registros = document.querySelector('#nav_registros')
+const nav_perfil = document.querySelector('.perfil');
 
-const lanzarMenu = () =>{
+const lanzarMenuMobile = () =>{
     general_nav.classList.toggle('inactive_nav');
 };
 
-hamburgesa_menu.addEventListener('click', lanzarMenu);
+const lanzarMenuRegistros = () =>{
+    menu_registros.classList.toggle('inactive');
+    nav_registros.classList.toggle('active_nav');
+}
+const lanzarMenuPerfil = () =>{
+    menu_perfil.classList.toggle('inactive');
+}
+
+hamburgesa_menu.addEventListener('click', lanzarMenuMobile);
+nav_registros.addEventListener('click', lanzarMenuRegistros);
+nav_perfil.addEventListener('click', lanzarMenuPerfil)
