5588b272ae5f68aa655eb9143fd3d2bd713d0b34
DavidTrujillo123 <davot098@gmail.com>
2023-07-10 19:14:02 -0500

    Implemencion para busqueda de protistas

--- js/search.js
+++ js/search.js
@@ -1,32 +1,18 @@
-const buscar_btn = document.querySelector('.buscar_btn');
-const sin_resultados_container = document.querySelector('.sin_resultados');
-const ejem_cards_container = document.querySelector('.ejem_cards');
 const btn_vermas = document.getElementsByClassName('btn_vermas');
 
-const showNoFound = (flag) =>{
-    // logica para activar
-
-    if(flag){
-        // quita sin resultados
-        sin_resultados_container.classList.add('inactive');
-        ejem_cards_container.classList.remove('inactive')
-    }else{
-        sin_resultados_container.classList.remove('inactive');
-        ejem_cards_container.classList.add('inactive')
-    }
-}
-
-const goLogin = () =>{
+const goLogin = () => {
     let respuesta = confirm('Necesitas una cuenta para ver más. Inicia sesión o crea una');
-    if (respuesta) 
+    if (respuesta)
         window.location.href = '../html/login.html';
 }
 
-function putActionBtnVermas(){
+
+function putActionBtnVermas(flag) {
+
     for (let index = 0; index < btn_vermas.length; index++) {
-        btn_vermas[index].addEventListener('click', goLogin);        
+        btn_vermas[index].addEventListener('click', goLogin);
     }
+
 }
 
-buscar_btn.addEventListener('click', () => {showNoFound(true)});
 putActionBtnVermas();
