5ed9dd62785f6108383588abe575d0195d13fd43
DavidTrujillo123 <davot098@gmail.com>
2023-07-18 18:31:10 -0500

    Mejora de css e implementacion de ejemplo de protistas en home_user y search

--- js/search_login.js
+++ js/search_login.js
@@ -1,16 +1,12 @@
 const principal_container_ver_prot = document.querySelector('.principal_container_ver_prot');
 const btn_cerra_ver_prot = document.querySelector('.btn_cerrar');
-// const btn_vermas = document.getElementsByClassName('btn_vermas');
-const ejem_cards_info = document.getElementsByClassName('ejem_cards_info');
 
 const showProtist = () => {
     principal_container_ver_prot.classList.remove('inactive');
 }
 
-function putActionBtnVermas(flag) {
-    for (let index = 0; index < ejem_cards_info.length; index++) {
-        ejem_cards_info[index].addEventListener('click', showProtist);
-    }
+function putActionBtnVermas(cards) {
+    cards.addEventListener('click', showProtist);
 }
 
 function closeVermas() {
@@ -18,5 +14,3 @@ function closeVermas() {
 }
 
 btn_cerra_ver_prot.addEventListener('click', closeVermas);
-
-putActionBtnVermas();
--- js/src/home_user.js
+++ js/src/home_user.js
@@ -1,13 +1,13 @@
 // Obtener datos
-// const result = JSON.parse(localStorage.getItem('user'));
- 
-let nombre = result.usunombre;
-let apellido = result.usuapellido;
-
-
+const nombre = result.usunombre;
+const apellido = result.usuapellido;
 const landscape = document.querySelector('.landscape');
+const div_ejem_cards = document.querySelector('.ejem_cards');
+const rutaPagina = window.location.pathname;
+// let url = 'https://protosoft-api.azurewebsites.net';
+let url = 'http://localhost:8080';
 
-
+//#region Landscape
 
 function putNames() {
     // Crear el elemento <h1>
@@ -27,7 +27,7 @@ function putNames() {
     // Crear el elemento <p> para 'Susana Oria'
     const pElement = document.createElement('p');
     pElement.id = 'nombre_usuario';
-    pElement.textContent = nombre+" ";
+    pElement.textContent = nombre + " ";
 
     // Crear el elemento <span> para 'Oria'
     const spanElement3 = document.createElement('span');
@@ -45,4 +45,120 @@ function putNames() {
     // Agregar el elemento <h1> al documento (por ejemplo, al <body>)
     landscape.appendChild(h1Element);
 }
-putNames();
+
+//#endregion
+
+
+//#region Protistas descatados
+//Funcion que pone la infomacion cuando se da click
+function llenarInformacion(nombreComun, imgSrc, reino, filo, clase, sobrenombre, orden, familia, genero, autor, descripcion, ubicacionHabitat) {
+    // Llenar información del div "ver_prot_inicio"
+    let nombreComunElement = document.getElementById("nombre_comun");
+    nombreComunElement.textContent = nombreComun;
+  
+    // Llenar información de la imagen
+    let imgProtistaElement = document.getElementById("img_protista");
+    imgProtistaElement.src = imgSrc;
+  
+    // Llenar información de los divs "container_pricipal_info"
+    document.getElementById("ver_prot_reino").textContent = reino;
+    document.getElementById("ver_prot_filo").textContent = filo;
+    document.getElementById("ver_prot_clase").textContent = clase;
+    document.getElementById("ver_prot_sobrenombre").textContent = sobrenombre;
+    document.getElementById("ver_prot_orden").textContent = orden;
+    document.getElementById("ver_prot_familia").textContent = familia;
+    document.getElementById("ver_prot_genero").textContent = genero;
+    document.getElementById("nombre_autor").textContent = autor;
+  
+    // Llenar información de los divs "container_secondary_info"
+    document.getElementById("ver_prot_descripcion").textContent = descripcion;
+    document.getElementById("ver_prot_habitat").textContent = ubicacionHabitat;
+  }
+
+//Funcion creadora de imagenes al inicio
+function createCardsEjem(element) {
+
+    let nombre_cientifico = element.nombre_cientifico;
+    let src_img = element.ruta_imagen;
+
+    let ejemCardsInfo = document.createElement("div");
+    ejemCardsInfo.className = "ejem_cards_info";
+
+    let p = document.createElement("p");
+    p.textContent = `${nombre_cientifico}`;
+
+    let img = document.createElement("img");
+    img.src = `${src_img}`;
+    img.alt = `ejemplo de una ${nombre_cientifico}`;
+
+    ejemCardsInfo.appendChild(p);
+    ejemCardsInfo.appendChild(img);
+
+    ejemCardsInfo.addEventListener('click', () => {
+        llenarInformacion(element.nombre_cientifico, 
+            element.ruta_imagen, 
+            element.reino,
+            element.filo, 
+            element.clase, 
+            element.nombre_vulgar, 
+            element.orden, 
+            element.familia, 
+            element.genero,
+            element.usunombre, 
+            element.descripcion, 
+            element.habitat);
+    });
+
+    div_ejem_cards.appendChild(ejemCardsInfo);
+
+    
+    putActionBtnVermas(ejemCardsInfo);
+}
+
+
+//funcion para obtener datos
+const getDataRegisters = async (url) => {
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const result = await response.json();
+    return result;
+};
+
+async function getRegistes(url){
+    try {
+        const response = await getDataRegisters(`${url}/registers/users/ten`);
+        response.forEach(element => {
+            createCardsEjem(element);
+        });
+    } catch (error) {
+        console.log(error);
+        alert('Error interno del servidor');
+    }
+};
+
+async function getAllRegistes(url){
+    try {
+        const response = await getDataRegisters(`${url}/registers/users`);
+        response.forEach(element => {
+            createCardsEjem(element);
+        });
+    } catch (error) {
+        console.log(error);
+        alert('Error interno del servidor');
+    }
+};
+
+//#endregion
+
+
+if (rutaPagina === "/C:/Users/david/OneDrive/Escritorio/U/SEMESTRES/DISE%C3%91O/Prototipo/html/login/home_user.html" ||rutaPagina === "https://davidtrujillo123.github.io/protosoft_v1/html/login/home_user.html") {
+    getRegistes(url);
+    putNames();
+} else {
+    getAllRegistes(url);
+}
+
